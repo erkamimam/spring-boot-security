9c2d7ada6abd6f113746630e4456994ffb34f6e0
HP <aliimam@hacettepe.edu.tr>
2023-04-19 11:31:55 +0300

    spring-security

---
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+
+
+}
+
+
+
+
+
+
+
+
